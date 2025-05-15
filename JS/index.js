af6b239868e726c3b3cd09b1cfc4df4b4d3c205b
Murilo Macedo Silva <152343609+MuriloMacedoSilva@users.noreply.github.com>
2025-05-14 21:42:55 -0300

    .

---
+++ JS/index.js
@@ -0,0 +1,14 @@
+
+// EVENTO DE ANIMAÇÃO NO HEADER
+
+let header = document.getElementById("header")
+
+document.addEventListener("scroll", function() {
+    if (window.pageYOffset > 0) {
+        header.style.boxShadow = "3px 3px 3px rgba(0, 0, 0, 0.500)";
+    } else if (window.pageYOffset == 0){
+        header.style.boxShadow = "none"
+    }
+})
+// --------------------------------------------------
+
