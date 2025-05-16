c42ef8fea8f183592c6ac9e593530fb5a3dd8e2f
Murilo Macedo Silva <152343609+MuriloMacedoSilva@users.noreply.github.com>
2025-05-16 08:51:13 -0300

    .

--- JS/index.js
+++ JS/index.js
@@ -5,7 +5,7 @@ let header = document.getElementById("header")
 
 document.addEventListener("scroll", function() {
     if (window.pageYOffset > 0) {
-        header.style.boxShadow = "3px 3px 3px rgba(0, 0, 0, 0.500)";
+        header.style.boxShadow = "3px 3px 3px rgba(0, 0, 0, 0.22)";
     } else if (window.pageYOffset == 0){
         header.style.boxShadow = "none"
     }
